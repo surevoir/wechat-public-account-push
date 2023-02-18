7d89093d570bdd3da87c7c9883df0e41987a426b
surevoir <125750163+surevoir@users.noreply.github.com>
2023-02-18 23:28:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '大宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '	5Z8zz6yfHCNuVXpifskaT513J42cmajjMyeYzIl6F0o',
+      id: '	owOIU5xptJ0KdYgzXfyuDALb4IpI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'owOIU5xptJ0KdYgzXfyuDALb4IpI',
+      useTemplateId: '	5Z8zz6yfHCNuVXpifskaT513J42cmajjMyeYzIl6F0o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-18',
       festivals: [
